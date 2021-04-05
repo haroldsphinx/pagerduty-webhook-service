f679cd9055168687c322ebe816ddf1d2ddc889a0
Adedayo <adedayo.akinpelu@gmail.com>
2021-04-05 04:27:16 +0100

    Added more Libraries

--- main.go
+++ main.go
@@ -4,13 +4,32 @@ import (
 	"encoding/json"
 	"net/http"
 	"log"
-
-	webhook "github.com/haroldsphinx/pagerduty-webhook-service"
+	"github.com/namsral/flag"
+	"strings"
+	webhook "github.com/pagerduty-webhook-service"
 )
 
-func main() {
-	http.HandleFunc("/", incidentHandler())
-	http.ListenAndServe(":8080", nil)
+var (
+	flagSet       = flag.NewFlagSetWithEnvPrefix(os.Args[0], "GWP", 0)
+	listenAddress = flagSet.String("listen", ":8080", "Listening port for the webhook service")
+	secret        = flagSet.String("secret", "", "Secret of the webhook API")
+	provider      = flagSet.String("provider", "pager-duty")
+	allowedPaths  = flagSet.String("allowedPaths", "", "Comma separated list of the paths we want to accept from the client")
+)
+
+func validateRequiredFlags() {
+	isValid := true
+	if len(strings.TrimSpace(*provider)) == 0 {
+		log.Println("Required flag 'provider' not specified")
+		isValid = false
+	}
+
+	if !isValid {
+		fmt.Println("")
+		flagSet.Usage()
+		fmt.Println("")
+		panic("See Flag Usage")
+	}
 }
 
 func incidentHandler() http.HandlerFunc {
@@ -24,3 +43,27 @@ func incidentHandler() http.HandlerFunc {
 		log.Println(payload)
 	}
 }
+
+
+func main() {
+	flagSet.Parse(os.Args[1:])
+	validateRequiredFlags()
+	lowerProvider := strings.ToLower(*provider)
+
+	//split , into an array
+	allowedPathsArray := []string{}
+	if len(*allowedPaths) > 0 {
+		allowedPathsArray = strings.Split(*allowedPaths, ",")
+	}
+
+	log.Printf("Dapperlabs Webhook Service Started")
+	p, err := proxy.NewProxy(*upstreamURL, allowedPathsArray, lowerProvider, *secret, ignoredUsersArray)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	http.HandleFunc("/", incidentHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
---
+++ providers/provider.go
@@ -0,0 +1,36 @@
+package providers
+
+import (
+	"errors"
+	"strings"
+)
+
+const (
+	IncidentTrigger       = "incident.trigger"       // IncidentTrigger is sent when an incident is newly created/triggered
+	IncidentAcknowledge   = "incident.acknowledge"   // IncidentAcknowledge is sent when an incident is acknowledged by a user
+	IncidentUnAcknowledge = "incident.unacknowledge" // IncidentUnacknowledge is sent when an incident is unacknowledged due to its acknowledgement timing out
+	IncidentResolve       = "incident.resolve"       // IncidentResolve is sent when an incident has been resolved
+	IncidentAssign        = "incident.assign"        // IncidentAssign is sent when an incident has been assigned to another user. Often occurs in concert with an acknowledge
+	IncidentEscalate      = "incident.escalate"      // IncidentEscalate is sent when an incident has been escalated to another user in the same escalation chain
+	IncidentDelegate      = "incident.delegate"      // IncidentDelegate is sent when an incident has been reassigned to another escalation policy
+	IncidentAnnotate      = "incident.annotate"      // IncidentAnnotate is sent when a note is created on an incident.
+)
+
+type Event string
+
+type Provider interface {
+	GetHeaderKeys() []string
+	Validate(hook Hook) bool
+	GetOwner(hook Hook) string
+	GetProviderName() string
+}
+
+type Hook struct {
+	Payload []byte
+	Headers map[string]string
+	RequestMethod string
+}  
+
+func assertProviderImplementations() {
+	var _ Provider = (*PagerDutyProvider)(nil)
+}
